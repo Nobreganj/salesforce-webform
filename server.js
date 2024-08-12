c9150552e7d99c82e87f5b5ca153ffe5bbfb274c
Nobreganj <57805973+Nobreganj@users.noreply.github.com>
2024-08-12 23:52:26 +0100

    server.js

--- server.js
+++ server.js
@@ -4,9 +4,9 @@ const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// Middleware to parse JSON
+// Middleware to parse JSON and urlencoded form data
 app.use(express.json());
-app.use(express.urlencoded({ extended: true })); // Add this to parse form data
+app.use(express.urlencoded({ extended: true }));
 
 // Serve static files from the 'public' directory
 app.use(express.static('public'));
